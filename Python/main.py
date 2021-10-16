f6545862fe767d5b94016f073b052e9cbfcb8bdf
syedahalam <syedahalam@yahoo.com>
2021-10-16 02:13:21 -0400

    variable

--- Python/main.py
+++ Python/main.py
@@ -6,6 +6,30 @@
 #print("New lines can be created with a backslash and n.")
 #input("What is your name?")
 # print("Hello " + input("What is your name?\n"))
-str=input("What is you name?\n")
-print("I will tell you number of characters in your name!")
-print(len(str))
+# str=input("What is you name?\n")
+# print("I will tell you number of characters in your name!")
+# print(len(str))
+
+#Python Variables-------------------------
+
+# name = input("What is your name?")
+# print(name)
+
+
+# 🚨 Don't change the code below 👇
+a = input("a: ")
+b = input("b: ")
+# 🚨 Don't change the code above 👆
+
+####################################
+#Write your code below this line 👇
+c=a
+a=b
+b=c
+
+#Write your code above this line 👆
+####################################
+
+# 🚨 Don't change the code below 👇
+print("a: " + a)
+print("b: " + b)
