e927ba25725413f085b54614b6d0cf0b0512cf86
syedahalam <syedahalam@yahoo.com>
2021-10-16 02:34:29 -0400

    Completed day 1 by making Band generator model

--- Python/main.py
+++ Python/main.py
@@ -17,19 +17,32 @@
 
 
 # 🚨 Don't change the code below 👇
-a = input("a: ")
-b = input("b: ")
+# a = input("a: ")
+# b = input("b: ")
 # 🚨 Don't change the code above 👆
 
 ####################################
 #Write your code below this line 👇
-c=a
-a=b
-b=c
+# c=a
+# a=b
+# b=c
 
 #Write your code above this line 👆
 ####################################
 
 # 🚨 Don't change the code below 👇
-print("a: " + a)
-print("b: " + b)
+# print("a: " + a)
+# print("b: " + b)
+
+#project 1- Day 1- BAND NAME GENERATOR MODEL
+
+#1. Create a greeting for your program.
+#print("Welcome to the Band Name Generator.\n")
+#2. Ask the user for the city that they grew up in.
+# city_name = input("What's name of the city you grew up in?\n")
+#3. Ask the user for the name of a pet.
+# pet_name = input("Whats your pet's name?\n")
+#4. Combine the name of their city and pet and show them their band name.
+# print("Your band name could be " + city_name + ' ' + pet_name)
+#5. Make sure the input cursor shows on a new line, see the example at:
+#   https://band-name-generator-end.appbrewery.repl.run/
