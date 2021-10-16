3c39f09d4422b4e3fb3c8382c38aac70d5764acf
syedahalam <syedahalam@yahoo.com>
2021-10-15 23:32:59 -0400

    part2 done

--- code.py
+++ code.py
@@ -10,3 +10,27 @@ elif(time < 22):
     print("A day without sunshine is like, you know, night.")
 else:
     print("Burning the midnight oil!")
+
+# Part 2: I Came, I 'Saur, I Conquered
+
+angry = True
+bored = True
+hungry = True
+tired = True
+
+# Complete the prompt here
+# Example `if` statement:
+if(angry and hungry and bored):
+    print('T-Rex will eat Triceratops')
+elif(tired and hungry):
+    print('T-Rex will eat Lguanadon')
+elif(hungry and bored):
+    print('T-Rex will eat Stegasaurus')
+elif(tired):
+    print('T-Rex will go to sleep')
+elif(angry and bored):
+    print('T-Rex will fight with the Velociraptor.')
+elif(angry and bored):
+    print('T-Rex will roar.')
+else:
+    print("T-Rex gives a toothy smile")
